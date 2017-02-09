264ffed5e6212585c199d773440475651b3e875c
Abhishek <abhishek.singh7@mindtree.com>
2017-02-09 15:53:13 +0530

    Initial Commit

---
+++ GitMergingXCuserState/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitMergingXCuserState
+//
+//  Created by Admin User on 2/9/17.
+//  Copyright © 2017 mindtree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitMergingXCuserState/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitMergingXCuserState
+//
+//  Created by Admin User on 2/9/17.
+//  Copyright © 2017 mindtree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
